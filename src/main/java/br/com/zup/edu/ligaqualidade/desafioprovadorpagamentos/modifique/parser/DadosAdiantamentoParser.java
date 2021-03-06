cf53e52c26d8bf3e1d40b265b04367d98e98970e
Caio Albuquerque Melo <caio.alb.melo@gmail.com>
2021-03-06 17:17:37 -0300

    Removes unused method

--- src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/parser/DadosAdiantamentoParser.java
+++ src/main/java/br/com/zup/edu/ligaqualidade/desafioprovadorpagamentos/modifique/parser/DadosAdiantamentoParser.java
@@ -3,29 +3,11 @@ package br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.modifique.parser;
 import br.com.zup.edu.ligaqualidade.desafioprovadorpagamentos.pronto.DadosRecebimentoAdiantado;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DadosAdiantamentoParser {
-
-    public List<DadosRecebimentoAdiantado> parse(final List<String> infoAdiantamentos) {
-
-        List<DadosRecebimentoAdiantado> listaDadosRecebimentoAdiantado = new ArrayList<>();
-        for (String infoAdiantamento : infoAdiantamentos) {
-            final String[] adiantamento = infoAdiantamento.split(",");
-
-            int idTransacao = Integer.parseInt(adiantamento[0]);
-            BigDecimal taxa = new BigDecimal(adiantamento[1]);
-
-            listaDadosRecebimentoAdiantado.add(
-                    new DadosRecebimentoAdiantado(idTransacao, taxa)
-            );
-        }
-
-        return listaDadosRecebimentoAdiantado;
-    }
 
     public Map<Integer, DadosRecebimentoAdiantado> parseAndMapByIdTransacao(final List<String> infoAdiantamentos) {
         Map<Integer, DadosRecebimentoAdiantado> adiantamentoByIdTransacao = new HashMap<>();
